9f12f1d2655789545cea25f1440b983f3ee6177f
SoutaGeuberth <soutadev@gmail.com>
2023-10-09 00:37:42 -0500

    sleep change

--- run.ps1
+++ run.ps1
@@ -2,5 +2,5 @@ docker compose up -d
 docker exec config-0 mongosh --file /data/db/crudconfig.js
 docker exec crud1-0 mongosh --file /data/db/crud1.js
 docker exec crud2-0 mongosh --file /data/db/crud2.js
-sleep 5
+sleep 10
 docker exec crud-router mongosh --file /data/db/shard.js
